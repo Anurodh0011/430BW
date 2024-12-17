19814deb4b69e4ddbe220005e172b1cb1b210049
Anurodh Prasain <anurodhprasain0011@gmail.com>
2024-12-17 17:31:28 +0545

    Function Notes

--- JS/demo.js
+++ JS/demo.js
@@ -1,4 +1,4 @@
-// Ways of writing function
+// -----------------------------Ways of writing function
 
 // function sum() {
 //     let n1 = 1
@@ -8,9 +8,36 @@
 // }
 // sum()
 
-// 
-function sum(a, b) {
-    let sum = a + b
-    console.log(sum)
-}
-sum(3, 4)
+
+// function sum(a, b) {
+//     let sum = a + b
+//     console.log(sum)
+// }
+// sum(3, 4)
+
+// let sum = function (a, b) {
+//     return a + b
+// }
+// console.log(sum(3, 4))
+
+
+
+// let sum = function (a, b = 0) { //setting default value for b
+//     return a + b
+// }
+// console.log(sum(3))
+
+
+//             ES6 syntax for function:
+
+// let sum = (n1, n2) => {
+//     return n1 + n2
+// }
+// console.log(sum(2, 3))
+
+// let sum = n1 => {               //while taking single input no bracket needed for parameter.
+//     return n1
+// }
+
+
+//  let sum = n1 => n1             //If the functions performs only single operations.
