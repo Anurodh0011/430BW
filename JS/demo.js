782c237d1acd0051ca253df486317f513d9fed42
Anurodh Prasain <anurodhprasain0011@gmail.com>
2024-12-17 17:05:05 +0545

    Function

--- JS/demo.js
+++ JS/demo.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+// Ways of writing function
+
+// function sum() {
+//     let n1 = 1
+//     let n2 = 2
+//     let sum = n1 + n2
+//     console.log("Total: ", sum)
+// }
+// sum()
+
+// 
+function sum(a, b) {
+    let sum = a + b
+    console.log(sum)
+}
+sum(3, 4)
