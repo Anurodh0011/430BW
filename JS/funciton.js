7e4f4c5c6ae94a1c59fbfb43c63b6f7edfbfb79b
Anurodh Prasain <anurodhprasain0011@gmail.com>
2024-12-18 16:48:43 +0545

    Day 3

---
+++ JS/funciton.js
@@ -0,0 +1,43 @@
+// -----------------------------Ways of writing function
+
+// function sum() {
+//     let n1 = 1
+//     let n2 = 2
+//     let sum = n1 + n2
+//     console.log("Total: ", sum)
+// }
+// sum()
+
+
+// function sum(a, b) {
+//     let sum = a + b
+//     console.log(sum)
+// }
+// sum(3, 4)
+
+// let sum = function (a, b) {
+//     return a + b
+// }
+// console.log(sum(3, 4))
+
+
+
+// let sum = function (a, b = 0) { //setting default value for b
+//     return a + b
+// }
+// console.log(sum(3))
+
+
+//             ES6 syntax for function:
+
+// let sub = (n1, n2) => {
+//     return n1 - n2
+// }
+// console.log(sub(2, 3))
+
+// let sum = n1 => {               //while taking single input no bracket needed for parameter.
+//     return n1
+// }
+
+
+//  let sum = n1 => n1 + n2           //If the functions performs only single operations.
