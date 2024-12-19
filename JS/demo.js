6b3ff6be7e5522f3cd8d3d68b3b4ab6f820bfd70
Anurodh Prasain <anurodhprasain0011@gmail.com>
2024-12-19 22:35:25 +0545

    ternary operator

--- JS/demo.js
+++ JS/demo.js
@@ -3,4 +3,12 @@ a = "I am John" +
 console.log(a)
 
 a = 0
-a > 1 ? console.log(`Hello`) : console.log(`hy`);
+a > 1 ? console.log(`Hello`) : console.log(`hy`); //if else using ternary operator.
+
+
+
+let age = 25;
+
+let ageGroup = age < 13 ? "Child" : age < 20 ? "Teenager" : age < 60 ? "Adult" : "Senior"
+
+console.log(ageGroup); // using more condition
