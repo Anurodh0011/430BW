122bd17615c66627e0d6bfa74c7f96c71188f25f
Anurodh Prasain <anurodhprasain0011@gmail.com>
2024-12-19 22:35:55 +0545

    ternary operator

--- JS/demo.js
+++ JS/demo.js
@@ -1,14 +1,14 @@
-a = "I am John" +
-    " Henry"
-console.log(a)
+// a = "I am John" +
+//     " Henry"
+// console.log(a)
 
-a = 0
-a > 1 ? console.log(`Hello`) : console.log(`hy`); //if else using ternary operator.
+// a = 0
+// a > 1 ? console.log(`Hello`) : console.log(`hy`); //if else using ternary operator.
 
 
 
-let age = 25;
+// let age = 25;
 
-let ageGroup = age < 13 ? "Child" : age < 20 ? "Teenager" : age < 60 ? "Adult" : "Senior"
+// let ageGroup = age < 13 ? "Child" : age < 20 ? "Teenager" : age < 60 ? "Adult" : "Senior"
 
-console.log(ageGroup); // using more condition
+// console.log(ageGroup); // using more condition for ternary operator
